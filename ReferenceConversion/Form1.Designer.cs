f1d3df95350a83bab5e49e319d6d1d2b75f409a9
TMBM26 <TMBM26@TMBM26>
2025-03-31 17:32:12 +0800

    Modify Form

--- ReferenceConversion/Form1.Designer.cs
+++ ReferenceConversion/Form1.Designer.cs
@@ -37,7 +37,9 @@
             groupBox1 = new GroupBox();
             Lb_Allowlist = new ListBox();
             Cbx_Project_Allowlist = new ComboBox();
+            groupBox2 = new GroupBox();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // Btn_GetFolder
@@ -62,23 +64,23 @@
             Lb_ShowAllCsproj.FormattingEnabled = true;
             Lb_ShowAllCsproj.HorizontalScrollbar = true;
             Lb_ShowAllCsproj.ItemHeight = 15;
-            Lb_ShowAllCsproj.Location = new Point(325, 91);
+            Lb_ShowAllCsproj.Location = new Point(8, 22);
             Lb_ShowAllCsproj.Name = "Lb_ShowAllCsproj";
-            Lb_ShowAllCsproj.Size = new Size(524, 214);
+            Lb_ShowAllCsproj.Size = new Size(522, 214);
             Lb_ShowAllCsproj.TabIndex = 2;
             // 
             // Tb_SaveFolder
             // 
-            Tb_SaveFolder.Location = new Point(500, 316);
+            Tb_SaveFolder.Location = new Point(181, 248);
             Tb_SaveFolder.Name = "Tb_SaveFolder";
             Tb_SaveFolder.Size = new Size(349, 23);
             Tb_SaveFolder.TabIndex = 3;
             // 
             // Btn_Convert
             // 
-            Btn_Convert.Location = new Point(357, 350);
+            Btn_Convert.Location = new Point(344, 357);
             Btn_Convert.Name = "Btn_Convert";
-            Btn_Convert.Size = new Size(75, 23);
+            Btn_Convert.Size = new Size(70, 25);
             Btn_Convert.TabIndex = 4;
             Btn_Convert.Text = "Convert";
             Btn_Convert.UseVisualStyleBackColor = true;
@@ -87,7 +89,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(330, 324);
+            label1.Location = new Point(8, 253);
             label1.Name = "label1";
             label1.Size = new Size(169, 15);
             label1.TabIndex = 5;
@@ -99,7 +101,8 @@
             groupBox1.Controls.Add(Cbx_Project_Allowlist);
             groupBox1.Location = new Point(44, 72);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(275, 254);
+            groupBox1.Padding = new Padding(5);
+            groupBox1.Size = new Size(275, 279);
             groupBox1.TabIndex = 6;
             groupBox1.TabStop = false;
             groupBox1.Text = "專案 白名單";
@@ -108,9 +111,9 @@
             // 
             Lb_Allowlist.FormattingEnabled = true;
             Lb_Allowlist.ItemHeight = 15;
-            Lb_Allowlist.Location = new Point(6, 51);
+            Lb_Allowlist.Location = new Point(8, 51);
             Lb_Allowlist.Name = "Lb_Allowlist";
-            Lb_Allowlist.Size = new Size(263, 199);
+            Lb_Allowlist.Size = new Size(259, 214);
             Lb_Allowlist.TabIndex = 8;
             // 
             // Cbx_Project_Allowlist
@@ -123,25 +126,38 @@
             Cbx_Project_Allowlist.Text = "請選取白名單專案";
             Cbx_Project_Allowlist.SelectedIndexChanged += Cbx_Project_Allowlist_SelectedIndexChanged;
             // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(Lb_ShowAllCsproj);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(Tb_SaveFolder);
+            groupBox2.Location = new Point(344, 72);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(5);
+            groupBox2.Size = new Size(538, 279);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "目前所有 csproj 檔案";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(884, 392);
+            ClientSize = new Size(936, 413);
+            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Controls.Add(label1);
             Controls.Add(Btn_Convert);
-            Controls.Add(Tb_SaveFolder);
-            Controls.Add(Lb_ShowAllCsproj);
             Controls.Add(Tb_ShowPath);
             Controls.Add(Btn_GetFolder);
-            MaximumSize = new Size(900, 600);
+            MaximumSize = new Size(1000, 1000);
             MinimumSize = new Size(635, 431);
             Name = "Form1";
             Padding = new Padding(5);
             Text = "Reference Convert";
             Load += Form1_Load;
             groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -157,5 +173,6 @@
         private GroupBox groupBox1;
         private ListBox Lb_Allowlist;
         private ComboBox Cbx_Project_Allowlist;
+        private GroupBox groupBox2;
     }
 }
